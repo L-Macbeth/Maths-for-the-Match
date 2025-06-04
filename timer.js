cbc1036bf4cf4a895ae806a24214826bde9172ce
Lillian <l.macbeth.2020@bristol.ac.uk>
2025-06-04 14:35:17 +0100

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,67 @@
+window.timerSteps = [
+  {
+    label: "Introduction",
+    description: "Welcome, explain the experiment, introduce footballs and pressure.",
+    duration: 5 * 60 // 5 min
+  },
+  {
+    label: "Experiment Video",
+    description: "Watch the experiment video and team footage.",
+    duration: 2 * 60 // 2 min
+  },
+  {
+    label: "Experiment Outline",
+    description: "Visual cue, outline experiment steps.",
+    duration: 0 // 0 min
+  },
+  {
+    label: "Active Experiment - Part 1",
+    description: "Groups drop and record bounces for first ball.",
+    duration: 3 * 60 // 3 min
+  },
+  {
+    label: "Swap Balls",
+    description: "Blow whistle, swap balls.",
+    duration: 30 // 30 sec
+  },
+  {
+    label: "Active Experiment - Part 2",
+    description: "Groups drop and record bounces for second ball.",
+    duration: 3 * 60 // 3 min
+  },
+  {
+    label: "Swap Balls",
+    description: "Blow whistle, swap balls.",
+    duration: 30 // 30 sec
+  },
+  {
+    label: "Return Balls",
+    description: "Students return balls.",
+    duration: 60 // 1 min
+  },
+  {
+    label: "Graph Plotting",
+    description: "Groups plot info on graph.",
+    duration: 3 * 60 // 3 min
+  },
+  {
+    label: "Discussion Questions",
+    description: "Discuss questions in groups.",
+    duration: 4 * 60 // 4 min
+  },
+  {
+    label: "Show Graph & Discussion",
+    description: "Show online graph and whole group discussion.",
+    duration: 5 * 60 // 5 min
+  },
+  {
+    label: "Extra Content",
+    description: "Discussion about research methodology.",
+    duration: 10 * 60 // 10 min
+  },
+  {
+    label: "End",
+    description: "Wrap up and finish.",
+    duration: 3 * 60 // 3 min
+  }
+];
